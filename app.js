8b4c93ece1cdd5671a4033ac0d47646157f6835a
zaryab <zaryabsheikh6@gmail.com>
2022-03-30 20:36:26 +0500

    Do while loop and functions practise

---
+++ app.js
@@ -0,0 +1,14 @@
+let i=0;
+while(i<5){
+    console.log(i);
+        i++;
+}
+let j=0;
+do{
+    console.log(j)
+    j++;
+}
+while(j<5);
+function clickbtn(){
+    alert("Hover on this picture to see the magic")
+}
